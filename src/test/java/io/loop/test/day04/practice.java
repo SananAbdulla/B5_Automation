89d53c0c0392806e27a2b1fe7dcc8ca4231d5c9e
SananAbdulla <sananabdulla@gmail.com>
2025-11-20 01:49:00 +0400

    first commit

---
+++ src/test/java/io/loop/test/day04/practice.java
@@ -0,0 +1,17 @@
+package io.loop.test.day04;
+
+import io.loop.test.utilities.DocuportConstants;
+import io.loop.test.utilities.GeneralConstants;
+import io.loop.test.utilities.WebDriverUtil;
+import org.openqa.selenium.WebDriver;
+
+
+public class practice {
+
+    public static void main(String[] args) {
+
+
+        WebDriver driver = WebDriverUtil.getDriver(GeneralConstants.CHROME);
+
+    }
+}
